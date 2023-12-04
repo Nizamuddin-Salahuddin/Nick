4b5f099cf77daa232d0ac7fa1f203c65af68a032
Nizam <er.nizamuddin08@gmail.com>
2023-12-04 14:13:05 +0530

    Testing Different Command

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           setPosition({latitude,longitude});
           try{
             const response= await axios.get(`https://api.opencagedata.com/geocode/v1/json?key=08ae2d8d6b824aa5bf4a62967b654925&q=${latitude}+${longitude}&pretty=1`)
-        
+        console.log(response);
             setCityName(response.data.results[0].formatted)
            }catch(error){
             console.log('Error getting city',error)
@@ -30,7 +30,7 @@ function App() {
 
   return (
     <div className="App">
-     <h1>Here the geolocation of user with Co-ordinates</h1>
+     <h1>Here the Geolocation of User With Co-ordinates</h1>
      {position?<p><b>latitude/longitude : </b> { position.latitude}/ { position.longitude}  </p>: <p>Not Found</p> }
 {cityName?<p><b>City Name : </b>{cityName}</p> :<p>Not Found</p>}
     </div>
